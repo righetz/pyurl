383e23ab51c82cf486bc4259976bced7f51492c7
righetz <righetz@mes3hacklab.org>
2015-07-17 05:28:50 +0200

    -added Http POST request option -added Http status code option -added HTTPError exception

--- pyurl.py
+++ pyurl.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python3
+#!/bin/python3
 """Simple CUrl porting for Python3
 """
 
 import urllib.request, re
 import sys
 import argparse
+from urllib.parse import urlencode
 
 def main():
     """"main method"""
@@ -12,15 +13,21 @@ def main():
     parser.add_argument('-o', metavar='outfile', help='Write output to file')
     parser.add_argument('-i', action='store_true', help='Show request headers')
     parser.add_argument('url', help='Define target URL')
+    parser.add_argument('-d', metavar='DATA', help='Http POST data between quotation marks')
+    parser.add_argument('-c', action='store_true', help='Show Http code')
+    print(parser.parse_args())
     control(parser.parse_args())
 
 def control(args):
     """arguments control and methods invoke"""
     headers = ""
+    post_data = None
     url = str(args.url)
+    if args.d != None:
+        post_data = data_post_format(args.d)
     if not re.match("http://", url):
         url = "http://" + url
-    text = get_file(url)
+    text = get_file(url, post_data, args.c)
     if args.i == True:
         if args.o != None:
             headers = get_headers(url)
@@ -34,25 +41,42 @@ def control(args):
     else:
         print(text)
 
-def connect(url):
+def connect(url, post_data):
     """connection method"""
     try:
         req = urllib.request.Request(
             url,
-            data=None,
             headers={"User-Agent" : "PyUrl V1.0"
                     }
             )
+        if post_data != None:
+            req.data = post_data.encode('utf-8')
         src = urllib.request.urlopen(req)
+    except urllib.error.HTTPError as err:
+        sys.exit(err)
     except urllib.error.URLError:
-        sys.exit("Could not resolve host %s\nCheck connection issues or format URL properly" % url)
+        sys.exit("Could not resolve host %s\nCheck your connection" % url)
     return src
 
-def get_file(url):
+def data_post_format(data_string):
+    """format input data to be handled by urllib.request"""
+    data_list = data_string.split("&")
+    data_map = {}
+    for dato in data_list:
+        temp = dato.split("=")
+        data_map[temp[0]] = temp[1]
+    return urlencode(data_map)
+
+def get_file(url, post_data, http_code):
     """print to stdout"""
-    src = connect(url)
+    src = connect(url, post_data)
     charset = src.headers.get_param('charset')
+    if not charset:
+        charset = 'latin-1'
+        # workaround for missing charset header data
     content = []
+    if http_code:
+        content.append("Http code: %d\n\n "% src.getcode())
     while True:
         line = src.readline()
         if line:
@@ -64,7 +88,7 @@ def get_file(url):
 
 def get_headers(url):
     """return URL headers"""
-    src = connect(url)
+    src = connect(url, None)
     return str(src.headers)
 
 def save_to_file(text, outfile, headers):
